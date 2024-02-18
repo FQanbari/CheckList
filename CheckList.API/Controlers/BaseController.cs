47936785279d1f2589cbe8361b7c301c3b08293d
FQanbari <fatemeh.ganbari1993@gmail.com>
2024-02-18 14:53:54 +0330

    Migrations

--- CheckList.API/Controlers/BaseController.cs
+++ CheckList.API/Controlers/BaseController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 namespace CheckList.API.Controlers;
+
+//dotnet ef migrations add Init_Read --context ReadDbContext --startup-project../CheckList.API/  -o Ef/Migrations
 
 [ApiController]
 [Route("api/[controller]")]
---
+++ CheckList.Infrastructure/Ef/Queries/Handler/SearchTravelerCheckListHandler.cs
@@ -0,0 +1,34 @@
+﻿using CheckList.Application.DTO;
+using CheckList.Application.Queries;
+using CheckList.Infrastructure.Ef.Contexts;
+using CheckList.Infrastructure.Ef.Models;
+using CheckList.Shared.Abstractions.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheckList.Infrastructure.Ef.Queries.Handler;
+
+internal sealed class SearchTravelerCheckListHandler : IQueryHandler<SearchTravelerCheckList, IEnumerable<TravelerCheckListDto>>
+{
+    private readonly DbSet<TravelerCheckListReadModel> _TravelerCheckLists;
+
+    public SearchTravelerCheckListHandler(ReadDbContext context)
+        => _TravelerCheckLists = context.TravelerCheckList;
+
+    public async Task<IEnumerable<TravelerCheckListDto>> HandleAsync(SearchTravelerCheckList query)
+    {
+        var dbQuery = _TravelerCheckLists
+            .Include(pl => pl.Items)
+        .AsQueryable();
+
+        if (query.SearchPhrase is not null)
+        {
+            dbQuery = dbQuery.Where(pl =>
+                Microsoft.EntityFrameworkCore.EF.Functions.Like(pl.Name, $"%{query.SearchPhrase}%"));
+        }
+
+        return await dbQuery
+            .Select(pl => pl.AsDto())
+            .AsNoTracking()
+            .ToListAsync();
+    }
+}
