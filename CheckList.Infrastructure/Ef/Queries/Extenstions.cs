4433549dc685ab949cc164760f43fdfae62b1034
FQanbari <fatemeh.ganbari1993@gmail.com>
2024-02-18 12:06:56 +0330

    Add extenstion for dto

---
+++ CheckList.Infrastructure/Ef/Queries/Extenstions.cs
@@ -0,0 +1,30 @@
+﻿using CheckList.Application.DTO;
+using CheckList.Infrastructure.Ef.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckList.Infrastructure.Ef.Queries;
+
+internal static class Extenstions
+{
+    public static TravelCheckListDto AsDto(this TravelerCheckListReadModel readModel)
+        => new()
+        {
+            Id = readModel.Id,
+            Name = readModel.Name,
+            Destination = new DestinationDto
+            {
+                City = readModel.Destination.City,
+                Country = readModel.Destination.Country,
+            },
+            Items = readModel.Items?.Select(x => new TravelItemDto
+            {
+                Name = x.Name,
+                Quantity = x.Quantity,
+                IsTaken = x.IsTaken
+            }),
+        };
+}
