ebed14f2dd10f9924be367b524251fc58ce53792
FQanbari <fatemeh.ganbari1993@gmail.com>
2024-02-18 12:59:06 +0330

    Add Dispatcher for command and query

---
+++ CheckList.Shared/Commands/InMemeoryCommandDispatcher.cs
@@ -0,0 +1,22 @@
+﻿
+using CheckList.Shared.Abstractions.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Input;
+
+namespace CheckList.Shared.Commands;
+
+internal sealed class InMemeoryCommandDispatcher : ICommandDispatcher
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public InMemeoryCommandDispatcher(IServiceProvider serviceProvider)
+        => _serviceProvider = serviceProvider;
+
+    public async Task DispatchAsync<TCommand>(TCommand command) where TCommand : class, Abstractions.Commands.ICommand
+    {
+        using var scope = _serviceProvider.CreateScope();
+        var handler = scope.ServiceProvider.GetRequiredService<ICommandHandler<TCommand>>();
+
+        await handler.HandleAsync(command);
+    }
+}
