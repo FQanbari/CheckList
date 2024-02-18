d0be43b128a9405887ba4e5680155c2ebcd830b6
FQanbari <fatemeh.ganbari1993@gmail.com>
2024-02-18 09:03:59 +0330

    Add reposotory interface to domain

---
+++ CheckList.Domain/Repositories/ITravelerCheckListRepository.cs
@@ -0,0 +1,12 @@
+﻿using CheckList.Domain.Entities;
+using CheckList.Domain.ValueObjects;
+
+namespace CheckList.Domain.Repositories;
+
+public interface ITravelerCheckListRepository
+{
+    Task<TravelerCheckList> GetAsync(TravelerCheckListId id);
+    Task AddAsync(TravelerCheckList travelerCheckList);
+    Task UpdateAsync(TravelerCheckList travelerCheckList);
+    Task DeleteAsync(TravelerCheckList travelerCheckList);
+}
