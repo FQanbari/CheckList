19f023d7e52e3d360c1e7fe2b118e6dd966d603e
FQanbari <fatemeh.ganbari1993@gmail.com>
2024-02-18 12:35:12 +0330

    Add dumb service

---
+++ CheckList.Infrastructure/Services/DumbWeatherService.cs
@@ -0,0 +1,11 @@
+﻿using CheckList.Application.DTO.External;
+using CheckList.Application.Services;
+using CheckList.Domain.ValueObjects;
+
+namespace CheckList.Infrastructure.Services;
+
+internal sealed class DumbWeatherService : IWeatherService
+{
+    public async Task<WeatherDto> GetWeatherAsync(Destination localization)
+        => await Task.FromResult(new WeatherDto(new Random().Next(5, 30)));
+}
